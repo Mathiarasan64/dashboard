217e92647735c0cd74dd836c14fc612af96410c5
Mathiarasan64 <125532699+Mathiarasan64@users.noreply.github.com>
2026-07-07 15:29:29 +0530

    Update data.py

--- data.py
+++ data.py
@@ -1,10 +1,29 @@
 import pandas as pd
 
-URL = "https://sheet.zohopublic.in/sheet/published/sfoej993e5907c3fb4e70a21047d22db57a9c?download=xlsx"
+URL = "https://sheet.zohopublic.in/sheet/published/sfoej993e5907c3fb4e70a21047d22db57a9c?download=csv&sheetname=Sheet1"
 
 def load_data():
-    try:
-        df = pd.read_excel(URL)
-        return df
-    except Exception as e:
-        return pd.DataFrame({"Error": [str(e)]})
+    df = pd.read_csv(URL)
+
+    money_cols = [
+        "Total price",
+        "Advance / amount paid",
+        "Pending",
+        "June",
+        "July",
+        "Total Payable Fee",
+        "Expected EMI Collection for June",
+        "Expected EMI Collection for July"
+    ]
+
+    for col in money_cols:
+        if col in df.columns:
+            df[col] = (
+                df[col]
+                .astype(str)
+                .str.replace(",", "", regex=False)
+                .replace("-", "0")
+            )
+            df[col] = pd.to_numeric(df[col], errors="coerce").fillna(0)
+
+    return df
