66b16354f2fe7ed8580a667b6a137874972e8641
Mathiarasan64 <125532699+Mathiarasan64@users.noreply.github.com>
2026-07-07 15:00:18 +0530

    Update data.py

--- data.py
+++ data.py
@@ -1,1 +1,10 @@
+import pandas as pd
 
+URL = "https://sheet.zohopublic.in/sheet/published/sfoej993e5907c3fb4e70a21047d22db57a9c"
+
+def load_data():
+    try:
+        tables = pd.read_html(URL)
+        return tables[0]
+    except Exception as e:
+        return pd.DataFrame({"Error": [str(e)]})
