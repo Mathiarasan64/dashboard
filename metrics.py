250ceea2726a3cc57610535746da934406fae4a3
Mathiarasan64 <125532699+Mathiarasan64@users.noreply.github.com>
2026-07-07 20:49:47 +0530

    Update metrics.py

--- metrics.py
+++ metrics.py
@@ -1,1 +1,9 @@
-
+{
+    "total_sales": ...,
+    "active_sales": ...,
+    "closed_sales": ...,
+    "one_shot_sales": ...,
+    "emi_sales": ...,
+    "pending": ...,
+    "collection_percentage": ...
+}
