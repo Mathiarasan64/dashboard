29454a3dd68e5b0dcb77d4a4dfec178cab5c6256
Mathiarasan64 <125532699+Mathiarasan64@users.noreply.github.com>
2026-07-07 15:06:13 +0530

    Update data.py

--- data.py
+++ data.py
@@ -1,10 +1,10 @@
 import pandas as pd
 
-URL = "https://sheet.zohopublic.in/sheet/published/sfoej993e5907c3fb4e70a21047d22db57a9c"
+URL = "https://sheet.zohopublic.in/sheet/published/sfoej993e5907c3fb4e70a21047d22db57a9c?download=xlsx"
 
 def load_data():
     try:
-        tables = pd.read_html(URL)
-        return tables[0]
+        df = pd.read_excel(URL)
+        return df
     except Exception as e:
         return pd.DataFrame({"Error": [str(e)]})
